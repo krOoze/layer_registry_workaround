1388ded35840d3cae22c3a8464b9d29cf08099d9
Petr Kraus <petr_kraus@email.cz>
2016-08-25 02:12:34 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# layer_registry_workaround
+Workaround for LunarG SDK 1.0.21 not puting layers in registry.
+
+Meant to be used only on Windows 10 x64, and with LunarG SDK 1.0.21.
+Run as an Administrator (RMB context menu)
---
+++ populateLayers.bat
@@ -0,0 +1,23 @@
+
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_api_dump.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_core_validation.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_image.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_object_tracker.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_parameter_validation.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_screenshot.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_swapchain.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_threading.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_unique_objects.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin\VkLayer_vktrace_layer.json /t REG_DWORD /d 0
+
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_api_dump.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_core_validation.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_image.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_object_tracker.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_parameter_validation.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_screenshot.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_swapchain.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_threading.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_unique_objects.json /t REG_DWORD /d 0
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Khronos\Vulkan\ExplicitLayers /v %VULKAN_SDK%\Bin32\VkLayer_vktrace_layer.json /t REG_DWORD /d 0
+
